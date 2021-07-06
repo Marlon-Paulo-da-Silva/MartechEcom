4bce88d8fc1d46545e8d184b3197069541d15a38
unknown <marlon.paulo.silva@outlook.com>
2021-07-06 02:41:30 -0300

    Inicio página Home

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+export function Home() {
+    return (
+        <div>
+            <section className="section destaque">
+                <div className="titulo">
+                    
+                </div>
+            </section>
+        </div>
+    )
+}
