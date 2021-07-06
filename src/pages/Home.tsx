dbed9c38161188287628c06d1eb6100487a93324
unknown <marlon.paulo.silva@outlook.com>
2021-07-06 03:05:12 -0300

    Estilizando página Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,9 @@ export function Home() {
         <div>
             <section className="section destaque">
                 <div className="titulo">
+                    <h1>Produtos em Destaque</h1>
+                </div>
+                <div className="produto-centro container">
                     
                 </div>
             </section>
