aa931457c51802a54960299a41a59ef5f176bd46
Marlon-Paulo-Da-Silva <marlon.paulo.silva@outlook.com>
2021-07-07 12:24:47 -0300

    Envelopando as páginas com AppProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { createContext, useState } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Home } from "./pages/Home";
 import { Signinlogin } from './pages/Signinlogin';
+import {AppProvider} from './context/cartContext'
 
 
 
@@ -11,9 +12,12 @@ function App() {
     
   
   return (
+
     <BrowserRouter>
+    <AppProvider>
       <Route path="/" exact component={Home}/>
       <Route path="/signinlogin" component={Signinlogin}/>
+      </AppProvider>
     </BrowserRouter>
     
   );
