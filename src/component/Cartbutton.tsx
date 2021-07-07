2e5245d4dd744ab87316233d59919e3ca2895ee7
unknown <marlon.paulo.silva@outlook.com>
2021-07-07 02:26:18 -0300

    tela de login iniciada

--- src/component/Cartbutton.tsx
+++ src/component/Cartbutton.tsx
@@ -1,11 +1,13 @@
-import { useContext } from 'react';
+import { useState ,useContext, createContext, ButtonHTMLAttributes } from 'react';
 import CartContext from '../App'
 
-export function Cartbutton(){
+type CartbuttonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function Cartbutton(props: CartbuttonProps){
 
   return(
     <div>
-      <button className="cartbutton">
+      <button className="cartbutton" {...props}>
         <span className="cart-icon"><i className='bx bx-cart-alt'></i></span>
         <span className="cart-qtde">1</span>
       </button>
--- src/pages/Signinlogin.tsx
+++ src/pages/Signinlogin.tsx
@@ -1,5 +1,33 @@
 export function Signinlogin() {
-  return(
-    <div> SigninLogin</div>
+
+  function handleCreateAccount() { }
+
+  return (
+    <div id="page-auth">
+      <main>
+        <div className="main-content">
+          <h2>Cadastrar-se</h2>
+          <form action="">
+            <input type="text" placeholder="E-mail" />
+            <button type="submit" onClick={handleCreateAccount}>Cadastrar</button>
+          </form>
+
+          <div className="separator">Já é cadastrado, faça o login </div>
+
+          <form action="">
+            <input
+              type="text"
+              placeholder="E-mail"
+            />
+            <input
+              type="text"
+              placeholder="Senha"
+            />
+            <button type="submit">Entrar</button>
+          </form>
+        </div>
+      </main>
+
+    </div>
   );
 }
