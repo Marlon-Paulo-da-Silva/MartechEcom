6a6a2666fb9d9f9acb9d4724d7394232070470ea
unknown <marlon.paulo.silva@outlook.com>
2021-07-06 23:58:21 -0300

    Criando uma nova página de produtos

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,31 +13,19 @@ export function Home() {
                 <Cartbutton></Cartbutton>
                 <div className="produto-centro container">
                     {products.map( (product) => (
-                       <div className="produto">
-                        <div className="cabecalho-produto">
-                            <img src={product.image} alt="" /></div> 
-                            <button className="add-carrinho"> Adicionar ao Carrinho <span><i className="bx bx-shopping-bag"></i></span></button>
-                            <ul className="icons">
-                                <span><i className="bx bx-heart"></i></span>
-                               
-                                <span><i className="bx bx-search"></i></span>
-                            </ul>
-                        <div className="rodape-produto">
-                            <a href="#">
-                                <h3> Relógio Quartz Masculino</h3>    
-                            </a>
+                       <div className="card">
+                           <img className="card_img" src={product.image} alt="product" />
 
-                            <div className="rating">
-                                <i className="bx bxs-star"></i>
-                                <i className="bx bxs-star"></i>
-                                <i className="bx bxs-star"></i>
-                                <i className="bx bxs-star"></i>
-                                <i className="bx bxs-star"></i>
-                            </div>
-
-                            <h4 className="price">R$60</h4>    
-                        </div> 
-                    </div>
+                           <div className="card_data">
+                               <h1 className="card_title">
+                                   {product.name}
+                               </h1>
+                               <span className="card_price">{product.price}</span>
+                               <p className="card_description">Lorem ipsum dolor consectetur adipisicing elit. Tenet</p>
+                               <a href="#" className="card_button">Comprar Agora</a>
+                               <a href="#" className="card_addtocart">Adicionar ao Carrinho</a>
+                           </div>
+                        </div>
                     ))}  
                 </div>
             </section>
