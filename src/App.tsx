9fc39174fc51bb2258edbeb4333080f2f5459d32
unknown <marlon.paulo.silva@outlook.com>
2021-07-08 07:14:34 -0300

    inicializando as funcionalidades do React Query

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,29 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import { Home } from "./pages/Home";
 import { Signinlogin } from './pages/Signinlogin';
 import {AppProvider} from './context/cartContext';
+import {useQuery} from 'react-query';
+
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import Badge from '@material-ui/core/Badge';
 
 
 
+export type CartItemType = {
+  id: number;
+  name: string;
+  image: string;
+  price: number;
+  amount: number;
+}
 
-
+const getProducts = async(): Promise<CartItemType> =>
+  await(await fetch("../Data/data")).json();
 
 function App() {
-    
-  
+    const {data, isLoading, error} = useQuery<CartItemType[]>('products', getProducts);
+    console.log( 'inicia', getProducts );
   return (
 
     <BrowserRouter>
