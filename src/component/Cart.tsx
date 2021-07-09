26c9ce414265782bcae18f3f6a3e22bca1fc78a6
unknown <marlon.paulo.silva@outlook.com>
2021-07-09 00:06:41 -0300

    component Cart criado

--- src/component/Cart.tsx
+++ src/component/Cart.tsx
@@ -1,5 +1,22 @@
+import CartItem from './CartItem';
+import { CartItemType } from '../pages/Home';
 
-
-export function Cart(){
-
+type Props = {
+    cartItems: CartItemType[];
+    addToCart: (clickedItem: CartItemType) => void;
+    removeFromCart: (id: number) => void;
 }
+
+const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart}) => {
+    return (
+        <div>
+            <h2>Seu Carrinho de Compras</h2>
+            {cartItems.length === 0 ? <p>Sem itens no carrinho</p> : null}
+            {cartItems.map(item  => (
+                <CartItem />
+            ))}
+        </div>
+    )
+};
+
+export default Cart;
